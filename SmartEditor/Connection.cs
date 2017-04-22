056fd3b3db88ed8b18e47177910b9c838949cf16
ioc0 <ioc0kernel@gmail.com>
2017-04-22 16:30:33 +0300

    Red box made

---
+++ SmartEditor/Connection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SmartEditor
+{
+    class Connection
+    {
+        private TcpListener tcpListener;
+        private Thread listenThread;
+
+        public void Server()
+        {
+            this.tcpListener = new TcpListener(IPAddress.Any, 45001);
+            this.listenThread = new Thread(new ThreadStart(ListenForClients));
+            this.listenThread.Start();
+        }
+
+        private void ListenForClients()
+        {
+            this.tcpListener.Start();
+            while (true)
+            {
+                TcpClient client = this.tcpListener.AcceptTcpClient();
+                //thread to handle communication
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clientThread.Start(client);
+            }
+        }
+
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+            byte[] message = new byte[4096];
+            int bytesRead;
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    //a socket error has occured
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    //the client has disconnected from the server
+                    break;
+                }
+
+                //message has successfully been received
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                System.Diagnostics.Debug.WriteLine(encoder.GetString(message, 0, bytesRead));
+            }
+
+            tcpClient.Close();
+        }
+    }
+
+}
